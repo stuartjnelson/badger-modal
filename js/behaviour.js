8724fc857ea52d88ac6a1063702ddf1b832ef33d
Stuart Nelson <stuart@weareready.com>
2019-06-20 17:07:05 +0100

    Adding back in multiple mdoals

--- js/behaviour.js
+++ js/behaviour.js
@@ -2,16 +2,16 @@ import BadgerModal from './badger-modal';
 import '../scss/_badger-modal.scss';
 
 
-const modal = new BadgerModal(".js-badger-modal", {
-    // onOpenFocusOnElement: '.js-focus-first'
-});
-
-
-// const modals = document.querySelectorAll(".js-badger-modal");
-
-// modals.forEach(modal => {
-//     new BadgerModal(modal);
+// const modal = new BadgerModal(".js-badger-modal", {
+//     // onOpenFocusOnElement: '.js-focus-first'
 // });
+
+
+const modals = document.querySelectorAll(".js-badger-modal");
+
+modals.forEach(modal => {
+    new BadgerModal(modal);
+});
 
 // setTimeout(() => {
 //     modal.openModal();
