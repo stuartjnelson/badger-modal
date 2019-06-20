278538d5d7b58004140132cc24a132a99b687434
Stuart Nelson <stuart@weareready.com>
2019-06-20 17:04:13 +0100

    PLaying with multiple modals

--- js/behaviour.js
+++ js/behaviour.js
@@ -1,9 +1,17 @@
 import BadgerModal from './badger-modal';
 import '../scss/_badger-modal.scss';
 
+
 const modal = new BadgerModal(".js-badger-modal", {
     // onOpenFocusOnElement: '.js-focus-first'
 });
+
+
+// const modals = document.querySelectorAll(".js-badger-modal");
+
+// modals.forEach(modal => {
+//     new BadgerModal(modal);
+// });
 
 // setTimeout(() => {
 //     modal.openModal();
